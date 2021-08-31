0814ee9b2f7db1e7d7e9e3e84b6291d7b55853f1
dawnaurora0117 <dawnaurora0117@gmail.com>
2021-08-31 22:31:45 +0900

    start project

---
+++ src/App.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="container">
+    <Header title="Task Tracker" />
+    <Tasks
+      @toggle-reminder="toggleReminder"
+      @delete-task="deleteTask"
+      :tasks="tasks"
+    />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Tasks from "./components/Tasks.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Tasks,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  methods: {
+    deleteTask(id) {
+      if (confirm("Are  you sure?")) {
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+      }
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      );
+    },
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "hello",
+        day: "day",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "hello",
+        day: "day",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: "hello",
+        day: "day",
+        reminder: false,
+      },
+    ];
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+</style>
